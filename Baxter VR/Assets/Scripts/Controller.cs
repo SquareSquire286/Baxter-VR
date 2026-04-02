7d64ba5875e669a8f433b8d01223fb8699d101ca
SquareSquire286 <sauer24j@uregina.ca>
2026-04-02 15:12:24 -0700

    Updates from Mar 17 demo day

---
+++ Baxter VR/Assets/Scripts/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Hand
+{
+    Right, // right beans
+    Left // left beans
+}
+
+public class Controller : MonoBehaviour // Controller beans
+{
+    [SerializeField] Hand hand;
+
+    public Hand GetHand()
+    {
+        return hand;
+    }
+}
